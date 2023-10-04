cff51d9c27e5e14ab35641b0c0abb41e26b02e0f
Oz017 <olekziembicki7@gmail.com>
2023-10-04 13:31:18 +0200

    Zadanie 1

---
+++ 04_10_2023/Zad1/zad1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad1</title>
+</head>
+<body>
+  <form method = "POST" action="zad1_2.php">
+    <input type="number" name="liczba">
+    <input type="submit" value="WYŚLIJ">
+  </form>
+</body>
+</html>
