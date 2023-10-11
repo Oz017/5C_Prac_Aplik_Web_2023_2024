e1457f6a7f528e9408accd17193896dc001f2aaa
Oz017 <olekziembicki7@gmail.com>
2023-10-11 15:09:29 +0200

    Szkielet strony pogoda.php

---
+++ 11_10_2023/pogoda.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl2.css">
+    <title>Prognoza pogody Wrocław</title>
+</head>
+<body>
+    <header></header>
+</body>
+</html>
