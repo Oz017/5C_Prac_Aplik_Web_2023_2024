ae9fc3652961e6a8b79b174c7f79737ce6d05f58
Oz017 <olekziembicki7@gmail.com>
2023-10-18 13:57:51 +0200

    Zadanie 1

---
+++ 18_10_2023/Zadanie1/Zad1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 1</title>
+</head>
+<body>
+    <form action="zad1_2.php" method="post">
+        <label for="imie">Podaj imię</label>
+        <input type="text" name="firstName" id="imie"></br>
+        <label for="nazwisko">Podaj nazwisko:</label>
+        <input type="text" name="lastName" id="nazwisko"></br>
+        <label for="rok">podaj Rok urodzenia</label>
+        <input type="number" name="year" id="rok"></br>
+        <label for="liczba">podaj liczbę publikacji:</label>
+        <input type="number" name="nr" id="liczba">
+        <input type="submit" value="Wyśij">
+    </form>
+</body>
+</html>
---
+++ 18_10_2023/Zadanie1/zad1_2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 1</title>
+</head>
+<body>
+    <?php
+        $host = "localhost";
+        $user = "root";
+        $db = "prognoza";
+        $pass = "";
+
+        $birthyear = $_POST["year"];
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $num = $_POST["nr"];
+
+        $connection = mysqli_connect($host, $user, $pass, $db);
+
+        $query = "INSERT INTO `matematycy`( `imie`, `nazwisko`, `rok_urodzenia`, `liczba_publikacji`) VALUES ( '$firstName', '$lastName', '$birthyear' , '$num' );";
+
+        $result = mysqli_query($connection,$query);
+
+        if ($result == TRUE){
+            echo "Utworzono nowy rekord";
+        }else{
+            echo "Błąd: " . $query . "</br>" . mysqli_error($connection);
+        }
+
+        mysqli_close($connection);
+    ?>
+</body>
+</html>
