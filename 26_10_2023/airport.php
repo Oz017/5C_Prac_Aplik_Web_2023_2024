775c16d37367094d7d78e36c2030b3a27d4fa7e0
Oz017 <olekziembicki7@gmail.com>
2023-10-26 16:00:42 +0200

    Kwerendy oraz szkielet strony airport.php

---
+++ 26_10_2023/airport.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odloty samolotów</title>
+    <link rel="stylesheet" href="styl6.css">
+</head>
+<body>
+    <header>
+        <section id="bannerLeft">
+            <h2>Odloty z lotniska</h2>
+        </section>
+
+        <section id="bannerRight">
+            <img src="zad6.png" alt="logotyp">
+        </section>
+    </header>
+    <main>
+        <h4>tabela odlotów</h4>
+        <table>
+            <tr>
+                <th>lp.</th>
+                <th>numer rejsu</th>
+                <th>czas</th>
+                <th>kierunek</th>
+                <th>status</th>
+            </tr>
+        </table>
+
+    </main>
+    <footer>
+        <section id="leftFooter">
+            <a href="kw1.jpg" target="_blank">Pobierz obraz</a>
+        </section>
+        <section id="midFooter">
+            <?php
+            
+            ?>
+        </section>
+        <section id="rightFooter">
+            Autor: Aleksander Ziembicki
+        </section>
+    </footer>
+</body>
+</html>
