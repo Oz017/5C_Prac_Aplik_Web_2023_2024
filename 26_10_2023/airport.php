c4703267b480fcfbd79a2da111b5c2e73cd4000e
Oz017 <olekziembicki7@gmail.com>
2023-11-02 14:30:39 +0100

    Dodanie skryptów do strony airport.php

--- 26_10_2023/airport.php
+++ 26_10_2023/airport.php
@@ -1,5 +1,9 @@
 <?php
-
+    $cookiename = "IsVisited";
+    $cookievalue = "Dzień Dobry!";
+    if(!isset($_COOKIE[$cookiename])){
+    setcookie($cookiename,$cookievalue,time()+3600);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -29,6 +33,29 @@
                 <th>kierunek</th>
                 <th>status</th>
             </tr>
+            <?php
+                $host = 'localhost';
+                $user = 'root';
+                $pass = '';
+                $db = 'egzamin';
+
+                $connect = mysqli_connect($host,$user,$pass,$db);
+
+                $query = 'SELECT id, nr_rejsu, czas, kierunek, status_lotu FROM `odloty` ORDER BY czas DESC;';
+
+                $result = mysqli_query($connect,$query);
+
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr>";
+                    echo "<td>". $row['id']."</td>";
+                    echo "<td>". $row['nr_rejsu']."</td>";
+                    echo "<td>". $row['czas']."</td>";
+                    echo "<td>". $row['kierunek']."</td>";
+                    echo "<td>". $row['status_lotu']."</td>";
+                    echo "</tr>";
+                }
+                mysqli_close($connect);
+            ?>
         </table>
 
     </main>
@@ -38,7 +65,9 @@
         </section>
         <section id="midFooter">
             <?php
-                echo '<p> OK</p>';
+                if(!isset($_COOKIE[$cookiename])){
+                    echo "<p><i>Dzień dobry! Sprawdź regulamin naszej strony</i></p>";
+                }else echo "<p><b>Miło nam, że nas znowu odwiedziłeś</b></p>";
             ?>
         </section>
         <section id="rightFooter">
