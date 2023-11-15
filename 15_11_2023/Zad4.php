b23a68461c4b46e0aa762a6967b4b90a880c560a
Oz17 <olekziembicki7@gmail.com>
2023-11-15 15:03:57 +0100

    Zadanie 4

---
+++ 15_11_2023/Zad4.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 4</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Imie</td>
+            <td>Nazwisko</td>
+            <td>Data Urodzenia</td>
+        </tr>
+        <?php
+            $host = 'localhost';
+            $user = 'root';
+            $pas = '';
+            $db = 'imiona';
+
+            $conn = mysqli_connect($host,$user,$pas,$db);
+            $query = 'SELECT * FROM dane';
+
+            $result = mysqli_query($conn,$query);
+
+            while($row = mysqli_fetch_assoc($result)){
+                echo "<tr>";
+                echo "<td>". $row['ID'] . "</td>";
+                echo "<td>". $row['imie'] . "</td>";
+                echo "<td>". $row['nazwisko'] . "</td>";
+                echo "<td>". $row['dataUrodzenia'] . "</td>";
+                echo "</tr>";
+
+            }
+        
+        ?>
+    </table>
+</body>
+</html>
